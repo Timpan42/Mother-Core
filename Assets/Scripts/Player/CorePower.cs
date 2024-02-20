25e34d5118af25832938492011a1af0eafa72188
timpan42 <tim@fagerdal.se>
2024-02-20 21:21:37 +0100

    sug efekten funkar nu på andra object

--- Assets/Scripts/Player/CorePower.cs
+++ Assets/Scripts/Player/CorePower.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CorePower : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float pullForce; 
+    [SerializeField] private Transform coreHolder;
+    private AffectedByCore affectScriptInCore;
+
+    private void OnTriggerEnter(Collider collisionObject) {
+        Debug.Log("object is in core range");
+        affectScriptInCore = collisionObject.GetComponent<AffectedByCore>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerStay(Collider collisionObject) {
+        Vector3 newDistans = CalculateDistans(collisionObject.transform);
+        affectScriptInCore.pulledByCore(newDistans, pullForce);
     }
+
+    private Vector3 CalculateDistans(Transform collisionObject){
+        float xDistans = coreHolder.position.x - collisionObject.position.x;
+        float yDistans = coreHolder.position.x - collisionObject.position.y;
+        float zDistans = coreHolder.position.x - collisionObject.position.z;
+
+        
+        return new Vector3(xDistans, yDistans, zDistans);
+    }
+
 }
