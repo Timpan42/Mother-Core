3be8a13b3c209d465739688070d107ef8191b482
timpan42 <tim@fagerdal.se>
2024-03-03 15:25:35 +0100

    nu kan ditect scriptet hita andra enemies

--- Assets/Scripts/WeaponDetection.cs
+++ Assets/Scripts/WeaponDetection.cs
@@ -1,7 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
+using UnityEngine.Video;
 
 
 public class WeaponDetection : MonoBehaviour
@@ -12,14 +18,19 @@ public class WeaponDetection : MonoBehaviour
     private InputAction combatModeInput;
 
     private bool abilityToFire = false;
-    private int ChangeTargetInputValue;
+    private float changeTargetInputValue;
     private bool combatMode = false;
 
-    [SerializeField] float radius;
-    [SerializeField] LayerMask layerToHit;
-    [SerializeField] Transform player;
-    Collider[] hitCollider = new Collider[10];
-    int numberOfColliders;
+    [SerializeField] private float radius;
+    [SerializeField] private LayerMask layerToHit;
+    [SerializeField] private Transform player;
+    private Collider[] hitCollider = new Collider[10];
+    private int numberOfColliders;
+    private int prevNumberOfColliders;
+
+    private bool switchFocusTarget = true;
+    private Collider getFocusedObjectCollider = null;
+    private int focusOnObject = 0;
 
     private void Awake()
     {
@@ -36,29 +47,192 @@ public class WeaponDetection : MonoBehaviour
         combatModeInput.Enable();
     }
 
+    // See radius
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(player.position, radius);
+    }
     private void Update()
+    {
+
+        ActivateCombatMode();
+
+        if (combatMode)
+        {
+            if (prevNumberOfColliders != numberOfColliders)
+            {
+                ClearArrayOnObjectExit();
+                prevNumberOfColliders = numberOfColliders;
+            }
+
+            numberOfColliders = Physics.OverlapSphereNonAlloc(player.position, radius, hitCollider, layerToHit);
+
+            SortArrayByDistends();
+            FocusObject();
+
+            FireAtTarget();
+            FireCoolDown();
+        }
+    }
+
+    private void ClearArrayOnObjectExit()
+    {
+        for (int i = 0; i < prevNumberOfColliders; i++)
+        {
+            hitCollider[i] = null;
+            Debug.Log("Clear");
+        }
+    }
+
+    private void ActivateCombatMode()
     {
         if (combatModeInput.WasPerformedThisFrame())
         {
             combatMode = !combatMode;
             Debug.Log("combatMode active");
         }
-        if (combatMode)
+    }
+
+    private void FireAtTarget()
+    {
+        if (fireInput.WasPerformedThisFrame())
         {
-            if (fireInput.WasPerformedThisFrame())
+            abilityToFire = true;
+            
+            // ask weapon script to attack 
+            
+            Debug.Log("FIRE!!!!");
+        }
+    }
+
+    private void FireCoolDown()
+    {
+        if (abilityToFire)
+        {
+            // ask weapon script for cool down
+        }
+    }
+
+    private void SortArrayByDistends()
+    {
+        if (numberOfColliders < 1)
+        {
+            return;
+        }
+        else
+        {
+            for (int i = 0; i < numberOfColliders; i++)
             {
-                abilityToFire = true;
-            }
-            if (abilityToFire)
-            {
-                numberOfColliders = Physics.OverlapSphereNonAlloc(player.position, radius, hitCollider, layerToHit);
-                for (int i = 0; i < numberOfColliders; i++)
+                int closestPositionIndex = i;
+                for (int j = i + 1; j < numberOfColliders; j++)
                 {
-                    Debug.Log("Collider: " + hitCollider[i].name);
+                    if (CheckSmallerDistance(closestPositionIndex, j))
+                    {
+                        closestPositionIndex = j;
+                    }
+                    if (closestPositionIndex != i)
+                    {
+                        var temp = hitCollider[i];
+                        hitCollider[i] = hitCollider[closestPositionIndex];
+                        hitCollider[closestPositionIndex] = temp;
+                    }
+
                 }
-                abilityToFire = false;
+
             }
+
         }
 
+    }
+
+    private bool CheckSmallerDistance(int smallestIndex, int nextIndex)
+    {
+        float small = Vector3.Distance(player.position, hitCollider[smallestIndex].transform.position);
+        float next = Vector3.Distance(player.position, hitCollider[nextIndex].transform.position);
+
+        if (small < next)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private void FocusObject()
+    {
+
+        if (changeTargetInput.WasPressedThisFrame())
+        {
+            changeTargetInputValue = changeTargetInput.ReadValue<float>();
+            switchFocusTarget = true;
+            changeObject();
+        }
+
+        if (switchFocusTarget)
+        {
+            getFocusedObjectCollider = hitCollider[focusOnObject];
+            switchFocusTarget = false;
+        }
+
+        if (hitCollider[focusOnObject] != null)
+        {
+            if (!switchFocusTarget)
+            {
+                FindOriginalFocusTarget();
+            }
+
+            Debug.DrawLine(player.position, hitCollider[focusOnObject].transform.position, Color.red);
+        }
+        else
+        {
+            switchFocusTarget = true;
+            focusOnObject = 0;
+        }
+    }
+
+    private void changeObject()
+    {
+
+        // om spelaren focusera redan på ett object hitta dens index i array
+        // bytt utt focusOnObject numer till dens index 
+
+        // om spelaren trycker Q eller E ändra focus start från 0  
+
+        if (changeTargetInputValue > 0)
+        {
+            focusOnObject++;
+
+            if (focusOnObject > numberOfColliders - 1)
+            {
+                focusOnObject = 0;
+            }
+
+            changeTargetInputValue = 0;
+        }
+        else if (changeTargetInputValue < 0)
+        {
+            focusOnObject--;
+            if (focusOnObject < 0)
+            {
+                focusOnObject = numberOfColliders - 1;
+            }
+
+            changeTargetInputValue = 0;
+        }
+    }
+
+    private void FindOriginalFocusTarget()
+    {
+        for (int i = 0; i < numberOfColliders; i++)
+        {
+            if (getFocusedObjectCollider == hitCollider[i])
+            {
+                focusOnObject = i;
+                break;
+            }
+        }
     }
 }
